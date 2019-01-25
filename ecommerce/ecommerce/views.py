9c7affea7cd69551412e21fc595bf8363687559b
e-landeros <landerosedgard@gmail.com>
2019-01-25 12:15:12 -0800

    playing with contact form validation

--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 def home_page(request):
 
     context = {
@@ -12,4 +14,17 @@ def about_page(request):
     return render(request, "home_page.html")
 
 def contact_page(request):
-    return render(request, "home_page.html")
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "form": contact_form,
+        'title': 'contact',
+        'content': 'welcome to the contact page'
+    }
+
+    if  contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
+    return render(request, "contact/view.html", context)
