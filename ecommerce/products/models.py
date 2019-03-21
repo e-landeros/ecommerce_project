747dce4c9fa42c79315e934c22dd6a0bf5ec689a
e-landeros <landerosedgard@gmail.com>
2019-03-21 13:38:47 -0700

    migrated prodcuts with image

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -4,7 +4,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(default=0.00, decimal_places=2, max_digits=20)
-    image = models.FileField(upload_to='products/')
+    image = models.FileField(upload_to='products/', null=True, blank=True)
     
     def __str__(self):
         return self.title
