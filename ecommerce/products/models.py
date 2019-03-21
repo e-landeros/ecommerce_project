53b1107d87a851c15f9f7a8ed54f8b705006e2ac
e-landeros <landerosedgard@gmail.com>
2019-03-21 13:36:45 -0700

    new product app and CBV's

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    price = models.DecimalField(default=0.00, decimal_places=2, max_digits=20)
+    image = models.FileField(upload_to='products/')
+    
+    def __str__(self):
+        return self.title
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,3 +1,17 @@
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = 'products/product_list.html'
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args,**kwargs)
+    #     print(context)
+    #     return context
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = 'products/product_detail.html'
