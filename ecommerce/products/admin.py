c1fc30a1dc300b0d6a8f5711c091445473263120
e-landeros <landerosedgard@gmail.com>
2019-03-21 19:43:54 -0700

    product image  upload path generator in models

--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('id','title',)
+    list_display_links = ('id', 'title',)
+
+    # def image_tag(self, obj):
+    #     return format_html('<img src="{}" />'.format(obj.image.url))
+
+    # image_tag.short_description = 'Image'
+
+
+admin.site.register(Product, ProductAdmin)
--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+import random 
+import os
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext 
+
+def upload_image_path(instance, filename):
+    print(instance)
+    print(filename)
+    new_filename = random.randint(1, 3910892383)
+    name, ext = get_filename_ext(filename)
+    final_filename = f'{new_filename}{ext}'
+    return f'products/{new_filename}/{final_filename}'
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(default=0.00, decimal_places=2, max_digits=20)
-    image = models.FileField(upload_to='products/', null=True, blank=True)
-    
+    image = models.FileField(upload_to=upload_image_path, null=True, blank=True)
+
     def __str__(self):
         return self.title
