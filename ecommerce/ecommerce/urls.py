2fa786a1b75008a939d864d7d2416547cab7fb9c
e-landeros <landerosedgard@gmail.com>
2019-03-29 22:26:03 -0700

    set up navbar

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -28,11 +28,11 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', home_page),
-    path('about/', about_page),
-    path('contact/', contact_page),
-    path('login/', login_page),
-    path('register/', register_page),
+    path('', home_page, name='home'),
+    path('about/', about_page, name='about'),
+    path('contact/', contact_page, name='contact'),
+    path('login/', login_page, name='login'),
+    path('register/', register_page, name='register'),
     path('admin/', admin.site.urls),
     
     path('products/', include(("products.urls", "products"),namespace="products"))
