b6fde711636acad34efa2998032b795a48a3f462
e-landeros <landerosedgard@gmail.com>
2019-05-19 00:49:51 -0700

    new shopping cart app models and views

--- ecommerce/carts/models.py
+++ ecommerce/carts/models.py
@@ -1,5 +1,15 @@
+from django.conf import settings
 from django.db import models
 
+from products.models import Product
+
+User = settings.AUTH_USER_MODEL
 class Cart(models.Model):
-    products = 
-    total = 
+    user        = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    products    = models.ManyToManyField(Product, blank=True)
+    total       = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from .models import Cart
+
 def cart_home(request):
+    cart_id = request.session.get("cart_id", None)
+    if cart_id is None: #and isinstance(cart_id, int):
+        print('create new cart')
+        request.session['cart_id'] = 12
+    else:
+        print('Cart ID exists')
     #print(request.session)
     #print(dir(request.session))
     #key = request.session.session_key
