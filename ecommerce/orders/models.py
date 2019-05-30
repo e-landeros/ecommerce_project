22a646941adec48a4b3052bc4525e0abfbd1f0e7
e-landeros <landerosedgard@gmail.com>
2019-05-29 22:18:01 -0700

    register orders in admin and migrate

--- ecommerce/orders/models.py
+++ ecommerce/orders/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from carts.models import Cart
+
+# stored value is left: displayed value on the right capitalized
+ORDER_STATUS_CHOICES = (
+    ('created', 'Created'),
+    ('paid', 'Paid'),
+    ('shipped', 'Shipped'),
+    ('refunded', 'Refunded'),
+)
+class Order(models.Model):
+    order_id        = models.CharField(max_length=120, blank=True)
+    # billing_profile = ?
+    # shipping address
+    # billing address
+    cart            = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    status          = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
+    shipping_total  = models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
+    total           = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+
+    def __str__(self):
+        return self.order_id
+
+# generate order_id
+# generate order total 
