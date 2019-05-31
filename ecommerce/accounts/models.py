de87d39e55a49f5d0f47167605a887fc032840fc
e-landeros <landerosedgard@gmail.com>
2019-05-30 23:31:54 -0700

    guest email view and db create

--- ecommerce/accounts/models.py
+++ ecommerce/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class GuestEmail(models.Model):
+    email       = models.EmailField(max_length=254)
+    active      = models.BooleanField(default=True)
+    update      = models.TimeField(auto_now=True)
+    timestamp   = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+    
