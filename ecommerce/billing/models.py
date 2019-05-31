82adf5cc4c78ea96377b76a4a67fac4b510dbd9f
e-landeros <landerosedgard@gmail.com>
2019-05-30 22:33:29 -0700

    billing profile model and auto generate signals

--- ecommerce/billing/models.py
+++ ecommerce/billing/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+# i want one user to have one biling profile when they sign up
+# auto make billing profile when user is created
+class BillingProfile(models.Model):
+    user        = models.ForeignKey(User, null=True, blank=True)
+    email       = models.EmailField(max_length=254)
+    active      = models.BooleanField(default=True)
+    update      = models.TimeField(auto_now=True)
+    timestamp   = models.TimeField(auto_now_add=True)
+    # I want to create customer_id for stripe or braintree here later
+
+    def __str__(self):
+        return self.email
+        
+# use signals to auto create billing profile on user creation         
+def user_created_reciever(sender, instance, created, *args, **kwargs):
+    if created:
+        BillingProfile.objects.get_or_create(user=instance)
+
+post_save.connect(user_created_reciever, sender=User)
